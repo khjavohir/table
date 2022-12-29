ef4f7347bca2fa247afee02bdede17a3a34573e9
Nasriddinbek Bektemirov <b.nasriddinbek@gmail.com>
2022-12-29 18:30:19 +0500

    finished table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Table from "./components/Table";
+import Table from "./components/table";
 
 const App = () => {
   const tableElements = [
-    { name: "id" },
-    { name: "client" },
-    { name: "product" },
-    { name: "price" },
+    { key: "id", name: "ID" },
+    { key: "client", name: "Name" },
+    { key: "product", name: "Products" },
+    { key: "price", name: "Total price" },
   ];
 
   const table = [
-    { id: 1, name: "Javohir", product: "rice", price: 5000 },
-    { id: 2, name: "Jalol", product: "potato", price: 5000 },
-    { id: 3, name: "Rasulbek", product: "tomato", price: 5000 },
+    { id: 1, client: "Javohir", product: "rice/potato/tomato", price: 5000 },
+    { id: 2, client: "Jalol", product: "potato/tomato", price: 5000 },
+    { id: 3, client: "Rasulbek", product: "tomato", price: 5000 },
   ];
 
   return (
     <div>
-      <Table table={table} />
+      <Table table={table} tableHeader={tableElements} />
     </div>
   );
 };
--- src/components/table.js
+++ src/components/table.js
@@ -1,15 +1,37 @@
 import '../css/table.css'
 
-const Table = ({ table }) => {
+const Table = ({ table, tableHeader }) => {
+
+
+
+
   return (
     <div>
       <table>
         <tr>
-          
+          {tableHeader && tableHeader.length > 0
+            ? tableHeader.map((item, index) => (
+                <th key={index}>
+                  <p>{item.name}</p>
+                </th>
+              ))
+            : ""}
         </tr>
+
+        {table && table.length
+          ? table.map((item, index) => (
+              <tr key={index}>
+                {tableHeader && tableHeader.length > 0 ? tableHeader.map((itemHeader, index) => (
+                  <td>{item[itemHeader.key]}</td>
+                )) : ''}
+                
+                
+              </tr>
+            ))
+          : ""}
       </table>
     </div>
-  )
+  );
 }
 
 export default Table
