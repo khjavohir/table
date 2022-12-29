f70940c9e24a6519c41bb21123d80039c80ce1a6
khjavohir <javohirxatamov2008@gmail.com>
2022-12-29 18:07:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
-import Table from "./components/table";
+import Table from "./components/Table";
 
 const App = () => {
+  const tableElements = [
+    { name: "id" },
+    { name: "client" },
+    { name: "product" },
+    { name: "price" },
+  ];
+
+  const table = [
+    { id: 1, name: "Javohir", product: "rice", price: 5000 },
+    { id: 2, name: "Jalol", product: "potato", price: 5000 },
+    { id: 3, name: "Rasulbek", product: "tomato", price: 5000 },
+  ];
+
   return (
     <div>
-      <Table />
+      <Table table={table} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/table.js
+++ src/components/table.js
@@ -1,54 +1,15 @@
-import "../css/table.css";
+import '../css/table.css'
 
-const Table = () => {
+const Table = ({ table }) => {
   return (
     <div>
       <table>
         <tr>
-          <th>
-            <p>Surname</p>
-          </th>
-          <th>
-            <p>Name</p>
-          </th>
-          <th>
-            <p>Adress</p>
-          </th>
-        </tr>
-
-        <tr>
-          <td>Xatamov</td>
-          <td>Javohir</td>
-          <td>Uzbekistan</td>
-        </tr>
-        <tr className="ex1">
-          <td>Cristiano</td>
-          <td>Ronaldo</td>
-          <td>Portugal</td>
-        </tr>
-        <tr>
-          <td>Maykl</td>
-          <td>Jordan</td>
-          <td>America</td>
-        </tr>
-        <tr className="ex1">
-          <td>Maykl</td>
-          <td>Jackson</td>
-          <td>America</td>
-        </tr>
-        <tr>
-          <td>Maykl</td>
-          <td>Jordan</td>
-          <td>America</td>
-        </tr>
-        <tr className="ex1">
-          <td>Maykl</td>
-          <td>Jackson</td>
-          <td>America</td>
+          
         </tr>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default Table;
+export default Table
